57f3d59610ffd6de77d844db676170be1cac74aa
Wanderson Lontra <wandersonlontra@yahoo.com.br>
2025-02-18 18:24:02 -0300

    feat: create fetcher to get currencies data

---
+++ internal/entity/currencies.go
@@ -0,0 +1,9 @@
+package entity
+
+type Currency struct {
+	Success   bool              	`json:"success"`
+	Timestamp int               	`json:"timestamp"`
+	Base      string            	`json:"base"`
+	Date      string            	`json:"date"`
+	Rates     map[string]float64 	`json:"rates"`
+}
---
+++ internal/web/fetcher.go
@@ -0,0 +1,44 @@
+package web
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/WandersonLontra/coin-cli/internal/entity"
+)
+
+type Fetcher struct {
+	BaseUrl 		string
+	Route 			string
+	AccessKey 		string
+	BaseCurrency 	string
+}
+
+func NewFetcher(baseUrl, route, accessKey, baseCurrency string) *Fetcher {
+	return &Fetcher{
+		BaseUrl: baseUrl,
+		Route: route,
+		AccessKey: accessKey,
+		BaseCurrency: baseCurrency,
+	}
+}
+
+func (f * Fetcher) GetEndpoint() string {
+	return fmt.Sprintf("%s%s?access_key=%s", f.BaseUrl, f.Route, f.AccessKey)
+}
+func (f *Fetcher) GetCurrencies() (*entity.Currency, error) {
+	endpoint := f.GetEndpoint()
+	res, err := http.Get(endpoint)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	currency := entity.Currency{}
+	err = json.NewDecoder(res.Body).Decode(&currency)
+	if err != nil {
+		return nil, err
+	}
+	return &currency, nil
+}
