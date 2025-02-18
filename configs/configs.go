02dfdcd64b13c6b94491f6a7024cd3dd412c9007
Wanderson Lontra <wandersonlontra@yahoo.com.br>
2025-02-18 18:25:03 -0300

    feat: configs file to get environment data

---
+++ configs/configs.go
@@ -0,0 +1,45 @@
+package configs
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/viper"
+)
+
+var (
+	BaseUrl 		string
+	AccessKey 		string
+	BaseCurrency 	string
+)
+
+func init() {
+	viper.SetDefault("BASE_CURRENCY", "USD")
+	viper.SetConfigName("configs")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(".")
+
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			fmt.Fprintf(os.Stderr, "Error reading config file: %s\n", err)
+			os.Exit(1)
+		}
+	}
+	viper.AutomaticEnv();
+
+	BaseUrl = viper.GetString("BASE_URL");
+	AccessKey = viper.GetString("ACCESS_KEY");
+	BaseCurrency = viper.GetString("BASE_CURRENCY");
+
+	if BaseUrl == "" {
+		fmt.Fprintln(os.Stderr, "Missing required BASE_URL environment variable")
+		os.Exit(1)
+	}
+
+	if AccessKey == "" {
+		fmt.Fprintln(os.Stderr, "Missing required ACCESS_KEY environment variable")
+		os.Exit(1)
+	}
+}
