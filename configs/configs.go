d7b843c48ea81576a7c90b0b4e30e327b7faf37a
Wanderson Lontra <wandersonlontra@yahoo.com.br>
2025-03-07 14:06:18 -0300

    fix: set cache directory according if it is dev mode or otherwise

--- configs/configs.go
+++ configs/configs.go
@@ -12,8 +12,9 @@ var (
 	BaseUrl 		string
 	AccessKey 		string
 	TTLCache		float64
+	IsDevMode		bool
+	CacheDir      	string
 	CacheFile = "cache.json"
-	CacheDir = "./cache_file/"
 )
 
 func init() {
@@ -23,13 +24,15 @@ func init() {
 		os.Exit(1)
 	}
 	exeDir := filepath.Dir(exePath)
-	
+
 	viper.SetConfigName("configs")
 	viper.SetConfigType("env")
 	viper.AddConfigPath(".")
 	viper.AddConfigPath(exeDir)
 
 	viper.SetDefault("TTL_CACHE_IN_HOURS", float64(12))
+	viper.SetDefault("DEV_MODE", false)
+
 	err = viper.ReadInConfig()
 
 	if err != nil {
@@ -43,6 +46,7 @@ func init() {
 	BaseUrl = viper.GetString("BASE_URL");
 	AccessKey = viper.GetString("ACCESS_KEY");
 	TTLCache = viper.GetFloat64("TTL_CACHE_IN_HOURS");
+	IsDevMode = viper.GetBool("DEV_MODE");
 
 	if BaseUrl == "" {
 		fmt.Fprintln(os.Stderr, "Missing required BASE_URL environment variable")
@@ -52,5 +56,11 @@ func init() {
 	if AccessKey == "" {
 		fmt.Fprintln(os.Stderr, "Missing required ACCESS_KEY environment variable")
 		os.Exit(1)
+	}
+
+	if IsDevMode {
+		CacheDir = "./cache_file";
+	} else {
+		CacheDir = exeDir + "/cache_file";
 	}
 }
